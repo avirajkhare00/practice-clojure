0742d45aa3c180f62b5765aa07c39fd12ce50b82
Aviraj Khare <avirajkhare00@gmail.com>
2020-08-16 22:52:21 +0530

    Aviraj | Some more progress

--- helloworld/src/helloworld/core.clj
+++ helloworld/src/helloworld/core.clj
@@ -9,6 +9,24 @@
 [a, b]
 (+ a b))
 
+(defn subtract
+"Returns subtraction of two numbers"
+[a, b]
+(- a b))
+
+(defn multiply
+"returns product of two numbers"
+[a, b]
+(* a b))
+
+(defn condtn
+"More complex function"
+[x]
+(if (> x 10)
+"this is a big number"
+"this is not so big number")
+)
+
 (defn -main
   "I can say 'Hello World'."
   []
@@ -20,4 +38,8 @@
   (do (println "Failure!")))
   (println some_list)
   (println some_set)
-  (println (add 2 3)))
+  (println (add 2 3))
+  (println (subtract 10 5))
+  (println (multiply 10 6))
+  (println (condtn 11))
+  (println (condtn 5)))
