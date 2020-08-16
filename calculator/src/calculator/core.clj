03da36f09364a16635e2311a34438f8d0658b0e9
Aviraj Khare <avirajkhare00@gmail.com>
2020-08-17 00:12:45 +0530

    Aviraj | Add code to read from stdin

--- calculator/src/calculator/core.clj
+++ calculator/src/calculator/core.clj
@@ -1,8 +1,29 @@
 (ns calculator.core)
+(require '[clojure.string :as str])
+
+(defn add
+"Returns sum of given two numbers"
+[a, b]
+(+ a b))
+
+(defn subtract
+"Returns subtraction of two numbers"
+[a, b]
+(- a b))
+
+(defn multiply
+"returns product of two numbers"
+[a, b]
+(* a b))
+
+(defn division
+"returns division"
+[a, b]
+(/ a b))
 
 (defn -main
   "I can say 'Hello World'."
   []
   (println "Simple calculator")
   (doseq [ln (line-seq (java.io.BufferedReader. *in*))]
-   (println ln)))
+   (println (str/split ln #" "))))
