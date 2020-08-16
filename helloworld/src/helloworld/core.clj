650d2eeda0b01bfc32e243554384aa6e908fb43f
Aviraj Khare <avirajkhare00@gmail.com>
2020-08-16 22:22:25 +0530

    Aviraj | Some progress

--- helloworld/src/helloworld/core.clj
+++ helloworld/src/helloworld/core.clj
@@ -1,6 +1,17 @@
 (ns helloworld.core)
 
+(def name "Aviraj Khare")
+(def some_list (list 1 2 3 4))
+
 (defn -main
   "I can say 'Hello World'."
   []
-  (println "Hello world! My name is Aviraj Khare."))
+  (println "Hello world! My name is")
+  (println name)
+  (println (+ 1 2 3 4 5))
+  (if false
+  (do (println "Success!")
+      "By Zeus's hammer!")
+  (do (println "Failure!")
+      "By Aquaman's trident!"))
+  (println some_list))
